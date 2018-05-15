c1df0e0c736e0d44a3f3ccafaa60f38a9a24330b
Alex Oro <zyacir@gmail.com>
2018-05-14 20:14:16 -0600

    Updated first sim

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
 var w = 400,
     h = 350,
-    angle_radius = 35;
-
-var svg = d3.select("#main")
-    .append("svg")
-    .attr("class", "sim")
-    .attr("width", w)
-    .attr("height", h);
-
-
-var mirror = svg.append("line")
-    .attr("x1", 0)
-    .attr("x2", w)
-    .attr("y1", 2*h/3)
-    .attr("y2", 2*h/3)
-    .attr("class", "mirror");
-
-var block = svg.append("rect")
-    .attr("x", 0)
-    .attr("y", 2*h/3)
-    .attr("width", w)
-    .attr("height", h/3)
-    .attr("class", "solid");
+    angle_radius = 35,
+    handle_radius = 10;
 
 var handles = [
-    {x: 60, y:70},
-    {x: 90, y:130}
-]
-
-var rays = [
-    {x1: 60, y1: 70, x2: 90, y2: 130},
-    { x1: 60, y1: 70, x2: 90, y2: 130 }
+    { x: 41, y: 129 },
+    { x: 112, y: 170 }
 ]
 
 var coords = {};
 
-function updateCoords(){
+function updateCoords() {
     coords.m = ((handles[0].y - handles[1].y) / (handles[0].x - handles[1].x))
 
     coords.x1 = handles[0].x;
@@ -54,12 +29,33 @@ function updateCoords(){
 
 updateCoords();
 
+var svg = d3.select("#main")
+    .append("svg")
+    .attr("class", "sim")
+    .attr("width", w)
+    .attr("height", h);
+
+
+var mirror = svg.append("line")
+    .attr("x1", 0)
+    .attr("x2", w)
+    .attr("y1", coords.h)
+    .attr("y2", coords.h)
+    .attr("class", "mirror");
+
+var block = svg.append("rect")
+    .attr("x", 0)
+    .attr("y", coords.h)
+    .attr("width", w)
+    .attr("height", h - coords.h)
+    .attr("class", "solid");
+
 svg.selectAll("circle")
     .data(handles)
     .enter().append("circle")
         .attr("cx", function(d){return d.x;})
         .attr("cy", function(d){return d.y;})
-        .attr("r", 10)
+        .attr("r", handle_radius)
         .attr("class", "handle")
         .call(d3.drag()
             .on("start", dragstarted)
@@ -107,27 +103,51 @@ svg.append("text")
 var angles = svg.append("path").attr("id", "arc");
 
 function updateAngles(){
+    theta = Math.abs(Math.atan(coords.m));
+
+    cost = Math.abs(Math.cos(theta));
+    sint = Math.abs(Math.sin(theta));
+
+    // FIXME
+    // Can this be cleaned up / refactored at all?
+
     angles.attr("d", "M " + coords.x3 + " " + coords.h
-        + " L " + (coords.x3 + angle_radius * Math.abs(Math.cos(Math.atan(coords.m)))) + " " + (coords.h - angle_radius * Math.abs(Math.sin(Math.atan(coords.m))))
-        + " A " + angle_radius + " " + angle_radius + " 0 0 1 " + (coords.x3 + angle_radius) + " " + coords.h
+        + " L " + (coords.x3 + angle_radius * cost) + " " + (coords.h - angle_radius * sint)
+        +" A " + angle_radius + " " + angle_radius + " 0 0 0 " + (coords.x3 - angle_radius * cost) + " " + (coords.h - angle_radius * sint)
         + "L " + coords.x3 + " " + coords.h
-        + " L " + (coords.x3 - angle_radius * Math.abs(Math.cos(Math.atan(coords.m)))) + " " + (coords.h - angle_radius * Math.abs(Math.sin(Math.atan(coords.m))))
-        + " A " + angle_radius + " " + angle_radius + " 0 0 0 " + (coords.x3 - angle_radius) + " " + coords.h
+        +" M " + (coords.x3 + 0.8 * angle_radius * Math.abs(Math.cos(((-Math.PI / 2) - theta) / 2))) + " " + (coords.h - angle_radius * 0.8 * Math.abs(Math.sin(((-Math.PI / 2) - theta) / 2)))
+        +" L " + (coords.x3 + 1.2 * angle_radius * Math.abs(Math.cos(((-Math.PI / 2) - theta) / 2))) + " " + (coords.h - angle_radius * 1.2 * Math.abs(Math.sin(((-Math.PI / 2) - theta) / 2)))
+        +" M " + (coords.x3 - 0.8 * angle_radius * Math.abs(Math.cos(((3*Math.PI / 2) - theta) / 2))) + " " + (coords.h - angle_radius * 0.8 * Math.abs(Math.sin(((3*Math.PI / 2) - theta) / 2)))
+        +" L " + (coords.x3 - 1.2 * angle_radius * Math.abs(Math.cos(((3*Math.PI / 2) - theta) / 2))) + " " + (coords.h - angle_radius * 1.2 * Math.abs(Math.sin(((3*Math.PI / 2) - theta) / 2)))
+        + " M " + coords.x3 + " " + coords.h 
+        + " L " + coords.x3 + " " + (coords.h - 1.2 * angle_radius)
         + " Z");
+
+        //  angles.attr("d", "M " + coords.x3 + " " + coords.h +
+        //      " L " + (coords.x3 + angle_radius * cost) + " " + (coords.h - angle_radius * sint) +
+        //      " A " + angle_radius + " " + angle_radius + " 0 0 0 " + (coords.x3) + " " + (coords.h - angle_radius) +
+        //      "L " + coords.x3 + " " + coords.h +
+        //      " L " + (coords.x3 - angle_radius * cost) + " " + (coords.h - angle_radius * sint) +
+        //      " A " + angle_radius + " " + angle_radius + " 0 0 1 " + (coords.x3) + " " + (coords.h - angle_radius) +
+        //      " M " + (coords.x3 + 0.8 * angle_radius * Math.abs(Math.cos(((-Math.PI / 2) - theta) / 2))) + " " + (coords.h - angle_radius * 0.8 * Math.abs(Math.sin(((-Math.PI / 2) - theta) / 2))) +
+        //      " L " + (coords.x3 + 1.2 * angle_radius * Math.abs(Math.cos(((-Math.PI / 2) - theta) / 2))) + " " + (coords.h - angle_radius * 1.2 * Math.abs(Math.sin(((-Math.PI / 2) - theta) / 2))) +
+        //      " M " + (coords.x3 - 0.8 * angle_radius * Math.abs(Math.cos(((3 * Math.PI / 2) - theta) / 2))) + " " + (coords.h - angle_radius * 0.8 * Math.abs(Math.sin(((3 * Math.PI / 2) - theta) / 2))) +
+        //      " L " + (coords.x3 - 1.2 * angle_radius * Math.abs(Math.cos(((3 * Math.PI / 2) - theta) / 2))) + " " + (coords.h - angle_radius * 1.2 * Math.abs(Math.sin(((3 * Math.PI / 2) - theta) / 2))) +
+        //      " M " + coords.x3 + " " + coords.h +
+        //      " L " + coords.x3 + " " + (coords.h - 1.2 * angle_radius) +
+        //      " Z");
 }
 
 updateAngles();
-
 
 function dragstarted(d){
     d3.select(this).raise().classed("active", true);
 }
 
 function dragged(d, i){
-    // console.log(i);
     d3.select(this)
-        .attr("cx", d.x = d3.event.x)
-        .attr("cy", d.y = i == 1 ? Math.max(d3.event.y, handles[0].y + 20) : Math.min(d3.event.y, handles[1].y - 20));
+        .attr("cx", d.x = clamp(d3.event.x, 0, w))
+        .attr("cy", d.y = i == 1 ? Math.max(Math.min(coords.h, d3.event.y), handles[0].y + 2 * handle_radius) : Math.min(Math.max(0,d3.event.y), handles[1].y - 2 * handle_radius));
 
     if(handles[0].x == handles[1].x) return;
 
---
+++ util.js
@@ -0,0 +1,3 @@
+function clamp(num, min, max) {
+    return Math.min(Math.max(num, min), max);
+}
